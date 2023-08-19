7eb8a466cb5ca7bcb41b650e1cb820efd258490f
DaiyanNDahy <daiyanndahy@gmail.com>
2023-08-19 22:02:53 +0600

    uploading data

---
+++ src/Addition.java
@@ -0,0 +1,10 @@
+public class Addition {
+
+    public static void main(String[] args){
+        int a=25,b=99;
+
+        int result=a+b;
+
+        System.out.println("The result is = "+ result);
+    }
+}
